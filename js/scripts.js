a52b09b58bd953ebe2411526342b336ff586b119
Megan Lee <melee124@gmail.com>
2018-03-12 04:56:44 -0700

    added javascript to replace appropriate numbers with Ping, Pong, or Pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,26 @@
 //Business logic
 
-
 $(document).ready(function() {
   $("form#countup").submit(function(event) {
     event.preventDefault();
     debugger;
     var number = parseInt($("input#number").val());
     var multiples = [];
-    var multiplelist = numbers.map(function(number) {
-      if (i % 3 === 0 && i % 5 === 0) {
-        return pingpong;
-      } else if (i % 3 === 0) {
-        return ping;
-      }
-    };
+    //var multiplelist = multiples.slice();
 
       for (var i = 0; i <= number; i += 1) {
-        if (i % i === 0) {
-          multiples.push(i);
-    //      multiplelist += "<li>" + multiples + "</li>";
-        };
+        if (i % 15 === 0) {
+          multiples.push("Pingpong");
+      } else if (i % 3 === 0) {
+          multiples.push("Ping");
+      } else if (i % 5 === 0) {
+        multiples.push("Pong");
+      } else {
+        multiples.push(i);
+      };
+      };
 
-    $('#result').text(multiplelist);
 
-
-
-  };
+    $('#result').text("<li>" + multiples + "</li>");
+  });
  });
-});
