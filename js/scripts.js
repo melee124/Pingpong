cb4dfb84335a70d326b85ca70215d5bbb32ef091
Megan Lee <melee124@gmail.com>
2018-03-11 22:09:35 -0700

    Changed javascript so numbers actually display.

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,21 @@ $(document).ready(function() {
     debugger;
     var number = parseInt($("input#number").val());
     var multiples = [];
+    var multiplelist = numbers.map(function(number) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        return pingpong;
+      } else if (i % 3 === 0) {
+        return ping;
+      }
+    };
 
       for (var i = 0; i <= number; i += 1) {
-        if (number % i === 0) {
+        if (i % i === 0) {
           multiples.push(i);
+    //      multiplelist += "<li>" + multiples + "</li>";
         };
 
-    $('#result').text(multiples);
+    $('#result').text(multiplelist);
 
 
 
