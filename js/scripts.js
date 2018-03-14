3216a6b7349b050cc0244e6c29db13547644814e
Megan Lee <melee124@gmail.com>
2018-03-13 22:21:16 -0700

    Rearranged business and UI logic, and added function and second array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,33 @@
+function.countup("#usernumber") {
+for (var i = 0; i <= number; i += 1) {
+  if (i % 15 === 0) {
+    firstarray.push("Pingpong");
+} else if (i % 3 === 0) {
+    firstarray.push("Ping");
+} else if (i % 5 === 0) {
+  firstarray.push("Pong");
+} else {
+  firstarray.push(i);
+};
+};
+
 //Business logic
 
 $(document).ready(function() {
   $("form#countup").submit(function(event) {
     event.preventDefault();
     debugger;
-    var number = parseInt($("input#number").val());
-    var multiples = [];
-    //var multiplelist = multiples.slice();
-
-      for (var i = 0; i <= number; i += 1) {
-        if (i % 15 === 0) {
-          multiples.push("Pingpong");
-      } else if (i % 3 === 0) {
-          multiples.push("Ping");
-      } else if (i % 5 === 0) {
-        multiples.push("Pong");
-      } else {
-        multiples.push(i);
-      };
-      };
+    var usernumber = parseInt($("input#usernumber").val());
+    var firstarray = [];
+    var secondarray = array.from(firstarray);
+      if (firstarray.isArray())
 
 
-    $('#result').text("<li>" + multiples + "</li>");
+
+
+
+
+
+    $('#result').text(secondarray);
   });
  });
